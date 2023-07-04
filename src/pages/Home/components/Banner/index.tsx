96d07ed80c9a41d5bddbe5c95b400e1e3d15690d
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2023-07-03 21:00:45 -0300

    implementado banner

--- src/pages/Home/components/Banner/index.tsx
+++ src/pages/Home/components/Banner/index.tsx
@@ -1,9 +1,33 @@
 import styles from './styles.module.scss';
+import Typed from 'react-typed';
+
+const strings = [
+    'React',
+    'React Native',
+    'Ionic',
+    'Redux',
+    'Expo',
+    'Angular 2+',
+    'Typescript',
+    'gaming',
+    'cats'
+]
 
 export function HomeBanner() {
     return (
         <section className={styles.section_banner}>
-
+            <div className={styles.container}>
+                <div className={styles.presentation_text}>
+                    Hi! I am front-end developer,
+                    talk to me about:
+                </div>
+                <Typed
+                    strings={strings}
+                    typeSpeed={200}
+                    loop
+                    className={styles.type_animation}
+                />
+            </div>
         </section>
     )
 }

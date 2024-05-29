623226313f657b6a82a2d39815ab19e01c45a95b
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-05-29 09:43:43 -0300

    test pdf-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { Home } from "./pages/Home"
+///@ts-ignore
+import * as pdfjsLib from "../node_modules/pdfjs-dist/build/pdf";
+///@ts-ignore
+import * as pdfjsWorker from "../node_modules/pdfjs-dist/build/pdf.worker.mjs";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 function App() {
 
